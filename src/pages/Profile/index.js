8db7897e645a47da65284ba263da4ff215fe59e4
HigorDenomar <higordenomar@hotmail.com>
2020-04-19 20:24:31 -0300

    routes created

---
+++ src/pages/Profile/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import {
+  View,
+  Text,
+} from 'react-native';
+
+// import styles from './styles';
+
+export default function Profile() {
+  return (
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center"}}>
+      <Text>Profile</Text>
+    </View>
+  );
+}
--- src/routes.js
+++ src/routes.js
@@ -1,16 +1,24 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator, CardStyleInterpolators } from '@react-navigation/stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+
+import Icon from '@expo/vector-icons/FontAwesome5'
 
 import Intro from './pages/Intro';
 import Home from './pages/Home';
 import Warning from './pages/Warning';
 import Register from './pages/Register';
 import Login from './pages/Login';
+import Profile from './pages/Profile';
 
 const Stack = createStackNavigator();
+const Tab = createBottomTabNavigator();
+const Drawer = createDrawerNavigator();
 
-function App() {
+
+export default function Routes() {
   return (
     <NavigationContainer>
       <Stack.Navigator screenOptions={{
@@ -33,10 +41,55 @@ function App() {
             headerShown: true,
           }}
         />
-        <Stack.Screen name="Home" component={Home} />
+        <Stack.Screen name="Home" component={Tabs} />
       </Stack.Navigator>
     </NavigationContainer>
   );
 }
 
-export default App;
+function Tabs() {
+  return(
+    <Tab.Navigator
+      tabBarOptions={{
+        activeTintColor: "#009688",
+        inactiveTintColor: "gray"
+      }}
+    >
+      <Tab.Screen
+        name="Feed"
+        component={Home}
+        options={{
+          tabBarLabel: "Home",
+          tabBarIcon: ({color, size}) => <Icon name="home" color={color} size={size} />,
+        }}
+      />
+
+      <Tab.Screen name="ProfileDrawer" component={Drawers}
+      options={{
+        tabBarLabel: "Eu",
+        tabBarIcon: ({color, size}) =>  <Icon name="user-alt" color={color} size={size} />,
+      }}/>
+    </Tab.Navigator>
+  )
+}
+
+function Drawers() {
+  return(
+    <Drawer.Navigator
+      drawerPosition="right"
+      drawerContentOptions={{
+        activeTintColor: "#009688",
+        inactiveTintColor: "gray",
+      }}
+    >
+      <Drawer.Screen
+        name="Profile"
+        component={Profile}
+        options={{
+          drawerLabel: "Eu",
+          drawerIcon: ({color}) =>  <Icon name="user-alt" color={color} size={18} />,
+        }}
+      />
+    </Drawer.Navigator>
+  )
+}
