6945a1a5ecc619944e24f083b9325d33b6617553
HigorDenomar <higordenomar@hotmail.com>
2020-04-19 20:23:44 -0300

    custom status bar

--- App.js
+++ App.js
@@ -1,8 +1,14 @@
 import React from 'react';
+import { StatusBar } from 'react-native';
 import Routes from './src/routes';
 
 export default function App() {
   return (
-    <Routes />
+    <>
+      <StatusBar
+        backgroundColor="#009688"
+      />
+      <Routes />
+    </>
   );
 }
